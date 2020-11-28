2d34489c1a43cdd9481e5365b22866d9ff6a639c
SkylarWen <skylarvane6729@gmail.com>
2020-11-28 14:57:38 -0800

    api calling

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,20 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
-import { BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+import CreateRoomPage from "./CreateRoomPage";
+import Room from "./Room";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default class HomePage extends Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return <Router>
-            <Switch>
-                <Route exact path="/" component={HomePage}><p>This is home page.</p></Route>
-                <Route exact path="/join" component={RoomJoinPage} />
-
-            </Switch>
-        </Router>;
-    }
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <p>This is the home page</p>
+          </Route>
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
+        </Switch>
+      </Router>
+    );
+  }
 }
--- frontend/urls.py
+++ frontend/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import index
 
 urlpatterns = [
-    path('',index),
-    path('join',index)
+    path('', index),
+    path('join', index),
+    path('create', index),
+    path('join/1', index),
+    path('room/<str:roomCode>', index)
+
 ]
